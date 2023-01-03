1e93bbe0ec96f004187cc792ac6bd41b0e76b330
dindin-nibnib <mail@dindin.ch>
2023-01-03 18:12:36 +0100

    fixed formatarray

--- lib/creation.cpp
+++ lib/creation.cpp
@@ -8,180 +8,180 @@
 #include "files.cpp"
 
 string toLowerNoNonAlnum(const string& text) {
-    string result;
-    for (char c : text) {
-        if (isalnum(c)) {
-            result += tolower(c);
-        }
-    }
-    return result;
+	string result;
+	for (char c : text) {
+		if (isalnum(c)) {
+			result += tolower(c);
+		}
+	}
+	return result;
 }
 
 string toSnakeCase(const string& text, bool screaming = false) {
-    string result;
-    for (char c : text) {
-        result += (c == ' ') ? '_' : screaming ? toupper(c) : tolower(c);
-    }
-    return result;
+	string result;
+	for (char c : text) {
+		result += (c == ' ') ? '_' : screaming ? toupper(c) : tolower(c);
+	}
+	return result;
 }
 
 string toPascalCase(const string& text) {
-    string result;
-    bool nextUpper = true;
-    for (char c : text) {
-        if (c == ' ') {
-            nextUpper = true;
-        } else if (nextUpper) {
-            result += toupper(c);
-            nextUpper = false;
-        } else {
-            result += tolower(c);
-        }
-    }
-    return result;
+	string result;
+	bool nextUpper = true;
+	for (char c : text) {
+		if (c == ' ') {
+			nextUpper = true;
+		} else if (nextUpper) {
+			result += toupper(c);
+			nextUpper = false;
+		} else {
+			result += tolower(c);
+		}
+	}
+	return result;
 }
 
 string formatArray(string s[]) {
-    string text;
-    int arraySize = s->size();
+	string text;
+	int arraySize = sizeof(*s) / sizeof(s[0]);
 
-    for (int i = 0; i < arraySize; i++) {
-        if (s[i].empty())
-            break;
+	for (int i = 0; i < arraySize; i++) {
+		if (s[i].empty())
+			break;
 
-        text += "\"" + s[i] + "\"";
-        if (i < arraySize - 1) {
-            if (!s[i + 1].empty()) {
-                text += ", ";
-            }
-        }
-    }
+		text += "\"" + s[i] + "\"";
+		if (i < arraySize - 1) {
+			if (!s[i + 1].empty()) {
+				text += ", ";
+			}
+		}
+	}
 
-    return text;
+	return text;
 }
 
-void createMod(const mod& mod) {
-    MOD_ID = mod.opt.id.empty() ? toLowerNoNonAlnum(mod.name) : mod.opt.id;
-    MOD_PATH = "../output/" + mod.name + "-bMM";
-    MOD_VERSION = mod.version;
-    TEMPLATE_PATH = "../templates/" + moddingApiMap[MODDING_API] + '-' + gameVersionMap[mod.gameVersion];
-    AUTHOR = toLowerNoNonAlnum(mod.author);
-    GAME_VERSION = mod.gameVersion;
-    MODDING_API = mod.moddingApi;
+void createMod(mod mod) {
+	MOD_ID = mod.opt.id.empty() ? toLowerNoNonAlnum(mod.name) : mod.opt.id;
+	MOD_PATH = "../output/" + mod.name + "-bMM";
+	MOD_VERSION = mod.version;
+	TEMPLATE_PATH = "../templates/" + moddingApiMap[MODDING_API] + '-' + gameVersionMap[mod.gameVersion];
+	AUTHOR = toLowerNoNonAlnum(mod.author);
+	GAME_VERSION = mod.gameVersion;
+	MODDING_API = mod.moddingApi;
 
-    map <string, string> modMap = {
-            {"MOD_VERSION", MOD_VERSION},
-            {"MOD_NAME", mod.name},
-            {"MOD_ID", MOD_ID},
-            {"AUTHOR", AUTHOR},
-            {"GAME_VERSION", gameVersionMap[mod.gameVersion]},
-            {"MOD_PACKAGE", AUTHOR + '.' + MOD_ID}
-    };
-    map <string, string> optModMap = {
-            {"MOD_DESCRIPTION", mod.opt.description},
-            {"MOD_URL", mod.opt.url},
-            {"MOD_UPDATE_URL", mod.opt.updateUrl},
-            {"MOD_CREDITS", mod.opt.credits},
-            {"MOD_LOGO_FILE", mod.opt.logoFile},
-            {"MOD_PARENT", mod.opt.parent},
-            {"AUTHORS", formatArray(mod.opt.authors)},
-            {"MOD_SCREENSHOTS", formatArray(mod.opt.screenshots)},
-            {"MOD_DEPENDENCIES", formatArray(mod.opt.dependencies)}
-    };
+	map <string, string> modMap = {
+					{"MOD_VERSION", MOD_VERSION},
+					{"MOD_NAME", mod.name},
+					{"MOD_ID", MOD_ID},
+					{"AUTHOR", AUTHOR},
+					{"GAME_VERSION", gameVersionMap[mod.gameVersion]},
+					{"MOD_PACKAGE", AUTHOR + '.' + MOD_ID}
+	};
+	map <string, string> optModMap = {
+					{"MOD_DESCRIPTION", mod.opt.description},
+					{"MOD_URL", mod.opt.url},
+					{"MOD_UPDATE_URL", mod.opt.updateUrl},
+					{"MOD_CREDITS", mod.opt.credits},
+					{"MOD_LOGO_FILE", mod.opt.logoFile},
+					{"MOD_PARENT", mod.opt.parent},
+					{"AUTHORS", formatArray(mod.opt.authors)},
+					{"MOD_SCREENSHOTS", formatArray(mod.opt.screenshots)},
+					{"MOD_DEPENDENCIES", formatArray(mod.opt.dependencies)}
+	};
 
-    // Cleanup old files before writing new ones
-    cleanup();
+	// Cleanup old files before writing new ones
+	cleanup();
 
-    // Copy everything from the template to ouput and replace all the variables in it
-    copyForF( TEMPLATE_PATH + "/mod", MOD_PATH);
-    replaceInForF(MOD_PATH, modMap);
-    replaceInForF(MOD_PATH, optModMap);
+	// Copy everything from the template to ouput and replace all the variables in it
+	copyForF(TEMPLATE_PATH + "/mod", MOD_PATH);
+	replaceInForF(MOD_PATH, modMap);
+	replaceInForF(MOD_PATH, optModMap);
 }
 
 void createItem(item item) {
-    string ITEM_ID = toSnakeCase(item.name);
-    string ITEM_CLASS = item.isBasic() ? "ItemBase" : toPascalCase(item.name);
-    string ITEM_FINAL = toSnakeCase(item.name, true);
+	string ITEM_ID = toSnakeCase(item.name);
+	string ITEM_CLASS = item.isBasic() ? "ItemBase" : toPascalCase(item.name);
+	string ITEM_FINAL = toSnakeCase(item.name, true);
 
-    map <string, string> itemMap = {
-            {"MOD_PACKAGE", AUTHOR + '.' + MOD_ID},
-            {"MOD_ID", MOD_ID},
-            {"AUTHOR", AUTHOR},
-            {"ITEM_ID", ITEM_ID},
-            {"ITEM_NAME", item.name},
-            {"ITEM_CLASS", ITEM_CLASS},
-            {"ITEM_DESCRIPTION", item.opt.desc[0]}
-    };
+	map <string, string> itemMap = {
+					{"MOD_PACKAGE", AUTHOR + '.' + MOD_ID},
+					{"MOD_ID", MOD_ID},
+					{"AUTHOR", AUTHOR},
+					{"ITEM_ID", ITEM_ID},
+					{"ITEM_NAME", item.name},
+					{"ITEM_CLASS", ITEM_CLASS},
+					{"ITEM_DESCRIPTION", item.opt.desc[0]}
+	};
 
-    // Paths
-    string tmpPath = "../tmp/" + ITEM_ID;
-    string itemPath = MOD_PATH + R"(\src\main\resources\assets\)" + MOD_ID;
-    string mainPath = MOD_PATH + R"(\src\main\java\com\)" + AUTHOR + '\\' + MOD_ID;
+	// Paths
+	string tmpPath = "../tmp/" + ITEM_ID;
+	string itemPath = MOD_PATH + R"(\src\main\resources\assets\)" + MOD_ID;
+	string mainPath = MOD_PATH + R"(\src\main\java\com\)" + AUTHOR + '\\' + MOD_ID;
 
-    // Copy everything from the template to tmp, replace everything in it, then copy it to the mod
-    copyForF(TEMPLATE_PATH + "/item", tmpPath);
-    replaceInForF(tmpPath, itemMap);
-    copyForF(tmpPath, MOD_PATH);
+	// Copy everything from the template to tmp, replace everything in it, then copy it to the mod
+	copyForF(TEMPLATE_PATH + "/item", tmpPath);
+	replaceInForF(tmpPath, itemMap);
+	copyForF(tmpPath, MOD_PATH);
 
-    // Add the item in iteminit
-    appendFileAfter(
-            "\n\tpublic static final Item " + ITEM_FINAL + " = new " + ITEM_CLASS + "(\"" + ITEM_ID + "\");\n",
-            mainPath + "/init/ItemInit.java", "();");
+	// Add the item in iteminit
+	appendFileAfter(
+		"\n\tpublic static final Item " + ITEM_FINAL + " = new " + ITEM_CLASS + "(\"" + ITEM_ID + "\");\n",
+		mainPath + "/init/ItemInit.java", "();");
 
-    // Add the item to the lang file
-    writeFile(
-            "\nitem." + ITEM_ID + ".name=" + item.name,
-            MOD_PATH + "/src/main/resources/assets/" + MOD_ID + "/lang/en_us.lang", true);
+	// Add the item to the lang file
+	writeFile(
+		"\nitem." + ITEM_ID + ".name=" + item.name,
+		MOD_PATH + "/src/main/resources/assets/" + MOD_ID + "/lang/en_us.lang", true);
 
-    // Copy texture
-    copyForF(item.texturePath, itemPath + R"(\textures\items\)" + ITEM_ID + ".png");
+	// Copy texture
+	copyForF(item.texturePath, itemPath + R"(\textures\items\)" + ITEM_ID + ".png");
 
-    if (ITEM_CLASS != "ItemBase") {
-        if (!item.opt.desc[0].empty()) {
-            string tooltip;
+	if (ITEM_CLASS != "ItemBase") {
+		if (!item.opt.desc[0].empty()) {
+			string tooltip;
 
-            for (const string& desc : item.opt.desc) {
-                if (!desc.empty())
-                    tooltip += "\n\t\ttooltip.add(\"" + desc + "\");";
-            }
+			for (const auto& desc : item.opt.desc) {
+				if (!desc.empty())
+					tooltip += "\n\t\ttooltip.add(\"" + desc + "\");";
+			}
 
-            appendFileAfter(
-R"(    @Override
+			appendFileAfter(
+				R"(    @Override
 	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn)
 	{)" + tooltip + R"(
 	})" + "\n\n", mainPath + "/objects/items/" + ITEM_CLASS + ".java", "ItemBase {");
-        }
-    }
+		}
+	}
 }
 
 void build(const string& s = "build") {
-    // Supprimer les anciens fichiers dans gradleready et passer les nouveaux
-    copyForF(MOD_PATH + "/src", GRADLE_PATH + "/src/");
-    copyForF(MOD_PATH + "/build.gradle", GRADLE_PATH + "/build.gradle");
+	// Supprimer les anciens fichiers dans gradleready et passer les nouveaux
+	copyForF(MOD_PATH + "/src", GRADLE_PATH + "/src/");
+	copyForF(MOD_PATH + "/build.gradle", GRADLE_PATH + "/build.gradle");
 
-    // Build
-    system("echo ----------------");
-    system(("cd " + GRADLE_PATH + " && .\\gradlew " + s).c_str());
-    system("echo ----------------");
+	// Build
+	system("echo ----------------");
+	system(("cd " + GRADLE_PATH + " && .\\gradlew " + s).c_str());
+	system("echo ----------------");
 
-    // Puis déplacer le mod qui vient d'être build dans l'output
-    if (s == "build") {
-        try {
-            string modJarName = MOD_ID + '-' + MOD_VERSION + ".jar";
-            string outputModPath = "../output/" + modJarName;
+	// Puis déplacer le mod qui vient d'être build dans l'output
+	if (s == "build") {
+		try {
+			string modJarName = MOD_ID + '-' + MOD_VERSION + ".jar";
+			string outputModPath = "../output/" + modJarName;
 
-            remove(outputModPath);
-            copy_file(GRADLE_PATH + "/build/libs/" + modJarName,
-                      outputModPath);
+			remove(outputModPath);
+			copy_file(GRADLE_PATH + "/build/libs/" + modJarName,
+				outputModPath);
 
-            // ree
-            string minecraftModPath = "C:/Users/micha/AppData/Roaming/.minecraft/mods/" + modJarName;
-            remove(minecraftModPath);
-            copy_file(outputModPath,minecraftModPath);
-        } catch (const std::exception& e) {
-            cout << "Build Failed";
-        }
-    }
+			// ree
+			string minecraftModPath = "C:/Users/micha/AppData/Roaming/.minecraft/mods/" + modJarName;
+			remove(minecraftModPath);
+			copy_file(outputModPath, minecraftModPath);
+		} catch (const std::exception& e) {
+			cout << "Build Failed";
+		}
+	}
 }
 
 #pragma clang diagnostic pop
